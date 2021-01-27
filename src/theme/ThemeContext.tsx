9ea72cf882b54ca6a5325cdada8272a07a3a50f9
malvinh1 <malvinhariyanto50@gmail.com>
2021-01-27 12:13:54 +0700

    add theme

---
+++ src/theme/ThemeContext.tsx
@@ -0,0 +1,21 @@
+import React, { useContext, useState } from "react";
+import { ThemeProvider as ThemeProviderEmotion } from "@emotion/react";
+import { lightTheme, darkTheme } from "./theme";
+
+const ThemeUpdateContext = React.createContext(() => {});
+
+export const useThemeUpdate = () => useContext(ThemeUpdateContext);
+
+export const ThemeProvider: React.FC = ({ children }) => {
+  const [useDarkTheme, setDarkTheme] = useState(false);
+
+  const toggleTheme = () => setDarkTheme(!useDarkTheme);
+
+  return (
+    <ThemeProviderEmotion theme={useDarkTheme ? darkTheme : lightTheme}>
+      <ThemeUpdateContext.Provider value={toggleTheme}>
+        {children}
+      </ThemeUpdateContext.Provider>
+    </ThemeProviderEmotion>
+  );
+};
---
+++ src/theme/theme.ts
@@ -0,0 +1,19 @@
+export const lightTheme = {
+  colors: {
+    background: "#fff",
+  },
+  spacing: {
+    s: 8,
+    m: 12,
+    l: 16,
+    xl: 24,
+    xxl: 30,
+  },
+};
+
+export const darkTheme = {
+  ...lightTheme,
+  colors: {
+    background: "#000",
+  },
+};
