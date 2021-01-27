71c4634a339d556ecce1210ed1476cb191d9fe04
malvinh1 <malvinhariyanto50@gmail.com>
2021-01-27 16:38:37 +0700

    update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
 /** @jsxImportSource @emotion/react */
 
 import { useTheme } from "@emotion/react";
-import React, { useState } from "react";
+import React from "react";
 
-import Logo from "../assets/logo.svg";
 import Card from "../components/Card";
 import Header from "../components/Header";
-import { useThemeUpdate } from "../theme/ThemeContext";
 
 export default function Home() {
   const theme = useTheme();
