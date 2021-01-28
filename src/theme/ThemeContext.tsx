61492f2103d3eefc751c61191ffd78dfa66b6935
malvinh1 <malvinhariyanto50@gmail.com>
2021-01-28 11:21:40 +0700

    implement local storage for theming

--- src/theme/ThemeContext.tsx
+++ src/theme/ThemeContext.tsx
@@ -3,20 +3,29 @@ import React, { useContext, useState } from "react";
 
 import { darkTheme, lightTheme } from "./theme";
 
+const ThemeContext = React.createContext(false);
 const ThemeUpdateContext = React.createContext(() => {});
 
 export const useThemeUpdate = () => useContext(ThemeUpdateContext);
+export const useThemeContext = () => useContext(ThemeContext);
 
 export const ThemeProvider: React.FC = ({ children }) => {
-  const [useDarkTheme, setDarkTheme] = useState(false);
+  const [useDarkTheme, setDarkTheme] = useState(
+    window.localStorage.getItem("theme") === "true" ? true : false
+  );
 
-  const toggleTheme = () => setDarkTheme(!useDarkTheme);
+  const toggleTheme = () => {
+    window.localStorage.setItem("theme", String(!useDarkTheme));
+    setDarkTheme(!useDarkTheme);
+  };
 
   return (
     <ThemeProviderEmotion theme={useDarkTheme ? darkTheme : lightTheme}>
-      <ThemeUpdateContext.Provider value={toggleTheme}>
-        {children}
-      </ThemeUpdateContext.Provider>
+      <ThemeContext.Provider value={useDarkTheme}>
+        <ThemeUpdateContext.Provider value={toggleTheme}>
+          {children}
+        </ThemeUpdateContext.Provider>
+      </ThemeContext.Provider>
     </ThemeProviderEmotion>
   );
 };
